745a4061714d11fed7cf1882c9cb0868eea494d4
samuel pandi hutan sinaga <136546770+samuelsinaga24102003@users.noreply.github.com>
2023-11-06 21:58:31 +0700

    Add files via upload

---
+++ quiz1.js
@@ -0,0 +1,13 @@
+const restaurant = {
+  name: "Tasyah Cafe",
+  city: "Perdagangan",
+  favoriteDrink: "Jus Alpukat",
+  favoriteFood: "Mie Ktiaw Goreng",
+  isVegan: true,
+};
+
+const name = restaurant.name;
+const favoriteDrink = restaurant.favoriteDrink;
+
+console.log("Nama Restoran:", restaurant.name);
+console.log("Minuman Favorit:", restaurant.favoriteDrink);
